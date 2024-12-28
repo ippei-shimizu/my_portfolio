3e3f81ccf3a82788a536498c972d0cc5987fd5db
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-12-29 02:25:43 +0900

    fix: サムネイル

--- app/article/note.ts
+++ app/article/note.ts
@@ -4,7 +4,6 @@ export const note = [
     title: "2024年 エンジニアライフ振り返り",
     url: "https://note.com/ippei_shimiuzu/n/n8672eed1e811",
     publishedAt: "2024.12.29",
-    thumbnail:
-      "https://assets.st-note.com/production/uploads/images/167625954/rectangle_large_type_2_4a666daf4f87f78a847fa6cf8a7f4442.png?fit=bounds&quality=85&width=1280",
+    thumbnail: "/2024_look_back.png",
   },
 ];
